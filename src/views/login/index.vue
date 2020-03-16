c2a353745c8857f0d053f3a30f762fd2611776b6
z18883658255 <228735133@qq.com>
2020-03-16 15:15:22 +0800

    登陆模块编辑

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,13 +7,100 @@
         <span class="titleline"></span>
         <span class="titlelogin">用户登录</span>
       </div>
+      <!-- 表单 -->
+      <div>
+        <el-form :rules="rules" class="loginform" ref="form" :model="form" label-width="0px">
+          <el-form-item>
+            <el-input prefix-icon="el-icon-user" placeholder="请输入手机号" v-model="form.phone"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item prop="logincode">
+            <!-- 使用栅格系统来添加内容 -->
+            <el-row>
+              <el-col :span="16">
+                <el-input prefix-icon="el-icon-key" placeholder="请输入验证码" v-model="form.logincode"></el-input>
+              </el-col>
+              <el-col :span="8">
+                <img class="loginCode" src="../../assets/login_captcha.png" alt />
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item class="checkboxheight" prop="isCheck">
+            <el-checkbox v-model="form.isCheck">
+              我已阅读并同意
+              <el-link type="primary">用户协议</el-link>和
+              <el-link type="primary">隐私条款</el-link>
+            </el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="loginbnt" type="primary" @click="onSubmit">登录</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="loginbnt" type="primary">注册</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
     <img class="rightimg" src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: {
+        // 手机号
+        phone: "",
+        // 密码
+        password: "",
+        // 验证码
+        logincode: "",
+        // 是否阅读
+        isCheck: []
+      },
+      rules: {
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" }
+        ],
+        logincode: [
+          { required: true, message: "验证码不能为空", trigger: "blur" },
+          { min: 4, max: 4, message: "验证的长度应该为 4", trigger: "blur" }
+        ],
+        isCheck: [
+          {
+            type: "array",
+            required: true,
+            message: "请勾选",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    // 点击登录按钮时触发
+    onSubmit() {
+      // 调用 form 的验证方法
+      this.$refs.form.validate(valid => {
+        // valid 就是验证后返回的数据
+        if (valid) {
+          // 验证通过的逻辑
+          this.$message({
+            message: "验证通过",
+            type: "success"
+          });
+        } else {
+          // 验证不通过的逻辑
+          this.$message.error("验证不通过");
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang="less">
@@ -61,6 +148,21 @@ export default {};
         font-weight: 400;
         color: rgba(12, 12, 12, 1);
       }
+    }
+    .loginform {
+      margin-top: 29px;
+    }
+    .loginCode {
+      height: 42px;
+      width: 100%;
+    }
+    .checkboxheight {
+      .el-form-item__content {
+        line-height: 20px;
+      }
+    }
+    .loginbnt {
+      width: 100%;
     }
   }
   .rightimg {
